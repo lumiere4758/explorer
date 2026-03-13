acc36bdeddcf157a2c4ba0d614c008e887cb56a0
lumiere4758 <avijaileo@gmail.com>
2026-03-13 17:48:29 +0530

    movement bug fix

--- script.js
+++ script.js
@@ -11,18 +11,14 @@ document.addEventListener("keydown", (e) => { keys[e.key.toLowerCase()] = true }
 document.addEventListener("keyup", (e) => { keys[e.key.toLowerCase()] = false })
 
 function update_player() {
-    if (keys["w"]) {
-        `if(keys["w"]) y -= speed
-        if(keys["s"]) y += speed
-        if(keys["a"]) x -= speed
-        if(keys["d"]) x += speed
-        player.style.left = x + "px"
-        player.style.top = y + "px"
-        requestAnimationFrame(update)`
-
-    }
+    if (keys["w"]) y -= speed
+    if (keys["s"]) y += speed
+    if (keys["a"]) x -= speed
+    if (keys["d"]) x += speed
+    player.style.left = x + "px"
+    player.style.top = y + "px"
+    requestAnimationFrame(update_player)
 }
 
-//call the function now
-
+//call the function noW
 update_player()
