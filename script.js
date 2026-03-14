24d063909cd18473e470dd59f43f418aa4e01807
lumiere4758 <avijaileo@gmail.com>
2026-03-14 17:01:21 +0530

    scatter images randomly on screen

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ function update_player() {
 
 //call the function noW
 update_player()
+
+//now put the images randomly evrywhere on the screen
+
+const imageFiles = ['boo.jpeg', 'book.jpeg', 'brain.jpeg', "coke.jpeg", 'colors.jpeg', 'crown.jpeg', 'diamond.jpeg', 'fries.jpeg', 'ipods.jpeg', 'moon.jpeg', 'moon2.jpeg', 'pixel.jpeg', 'star.jpeg'];
+
+const container = document.getElementById("stuff-container");
+
+imageFiles.forEach(fileName => {
+    const img = document.createElement('img');
+    img.src = `assets/${fileName}`;
+    img.className = `random-img`;
+    // random positions
+    const randomTop = Math.floor(Math.random() * 80) + 10;
+    const randomLeft = Math.floor(Math.random() * 80) + 10;
+
+    img.style.top = `${randomTop}%`;
+    img.style.left = `${randomLeft}%`;
+
+    container.appendChild(img);
+});
+
+//tired byeee
